427be5f6066d51c97ef98af18b83e0b1c0afa232
Lyhour <thong.lyhour@cyder.page>
2025-09-23 16:36:50 +0700

    add discovery service

---
+++ rtr-eureka-server/src/test/java/com/spring/hour/discovery/RtrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hour.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
